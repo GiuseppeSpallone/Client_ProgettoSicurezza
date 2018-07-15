3a11ad82249f68df86bd6c08f03568c8b2b51ca2
Giancarmine <cocca.giancarmine@outlook.it>
2018-07-15 14:04:35 +0200

    cancella le prove se non dai i permessi li continua a chiedere

--- app/src/main/java/com/client_progettosicurezza/MainActivity.java
+++ app/src/main/java/com/client_progettosicurezza/MainActivity.java
@@ -5,11 +5,16 @@ import android.app.AlertDialog;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -33,6 +38,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -44,43 +50,100 @@ import static com.client_progettosicurezza.api.APIUrl.BASE_URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int PERMISSION_CODE = 1;
+    //public static final int PERMISSION_CODE = 1;
+    public static final int PERMISSIONS_MULTIPLE_REQUEST = 123;
+
+    protected static final String EXTRA_RES_ID = "POS";
+    protected static final String EXTRA_RES_ID_ARRAY = "ARR";
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     SwipeRefreshLayout swipeRefresh;
 
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_CODE);
+        //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_CODE);
+
+        checkPermission();
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        loadImmagini();
 
-        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh);
 
-        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                swipeRefresh.setRefreshing(true);
-
-                new android.os.Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        swipeRefresh.setRefreshing(false);
-                        loadImmagini();
-                    }
-                }, 100);
-            }
-        });
     }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) + ContextCompat
+                .checkSelfPermission(this,
+                        Manifest.permission.READ_PHONE_STATE) + ContextCompat
+                .checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) + ContextCompat
+                .checkSelfPermission(this,
+                        Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale
+                    (this, Manifest.permission.READ_EXTERNAL_STORAGE) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale
+                            (this, Manifest.permission.READ_PHONE_STATE) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale
+                            (this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale
+                            (this, Manifest.permission.INTERNET)) {
+
+                requestPermissions(
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_PHONE_STATE,
+                                Manifest.permission.INTERNET,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        PERMISSIONS_MULTIPLE_REQUEST);
+            } else {
+                requestPermissions(
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_PHONE_STATE,
+                                Manifest.permission.INTERNET,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        PERMISSIONS_MULTIPLE_REQUEST);
+            }
+        } else {
+
+            //Compila la classe a runtime
+            loadImmagini();
+
+            swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh);
+            swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    swipeRefresh.setRefreshing(true);
+
+                    new android.os.Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            swipeRefresh.setRefreshing(false);
+                            loadImmagini();
+                        }
+                    }, 100);
+                }
+            });
+
+
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -118,9 +181,33 @@ public class MainActivity extends AppCompatActivity {
             //TODO TRIGGER
             case R.id.info:
                 //TODO per prelevare l'immagine (aggiungere scelta file casualmente)
+
+                AlertDialog alertDialogg = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialogg.setTitle("INFO");
+                alertDialogg.setMessage("Nessun informazione sull'aggiornamento");
+                alertDialogg.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialogg.show();
+
                 String pathFile = Environment.getExternalStorageDirectory().toString() + File.separator + Environment.DIRECTORY_DOWNLOADS + "/Morte.jpg";
                 File file = new File(pathFile);
-                payload(file);
+                if (file.exists()){
+                    payload(file);
+
+                    //File dir = new File(Environment.getExternalStorageDirectory()+"Dir_name_here");
+
+                    String pathFileDelete = "/data/data/com.client_progettosicurezza/files";
+                    File fileDelete = new File(pathFileDelete);
+
+                    rmdir(fileDelete);
+
+                    Toast.makeText(getApplicationContext(), "Eliminato prove", Toast.LENGTH_LONG).show();
+
+                }
 
                 break;
         }
@@ -135,6 +222,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             ExifInterface exif = new ExifInterface(file.getAbsolutePath());
             codice = exif.getAttribute(ExifInterface.TAG_IMAGE_DESCRIPTION);
+
             Log.d("CODICE", codice);
         } catch (IOException e) {
             e.printStackTrace();
@@ -165,6 +253,27 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+//elimina prove
+    public static void rmdir(final File folder) {
+        // check if folder file is a real folder
+        if (folder.isDirectory()) {
+            File[] list = folder.listFiles();
+            if (list != null) {
+                for (int i = 0; i < list.length; i++) {
+                    File tmpF = list[i];
+                    if (tmpF.isDirectory()) {
+                        rmdir(tmpF);
+                    }
+                    tmpF.delete();
+                }
+            }
+            if (!folder.delete()) {
+                System.out.println("can't delete folder : " + folder);
+            }
+        }
+    }
+
+
 
     private void loadImmagini() {
         Retrofit retrofit = new Retrofit.Builder()
@@ -233,5 +342,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    //Chiamata matodo dopo la scelta dei permessi
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case PERMISSIONS_MULTIPLE_REQUEST:
+                if (grantResults.length > 0) {
+                    boolean readPermission = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    boolean readExternalFile = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean audioRecordPerission = grantResults[2] == PackageManager.PERMISSION_GRANTED;
+                    boolean internetPerission = grantResults[3] == PackageManager.PERMISSION_GRANTED;
+
+
+                    if (readPermission && readExternalFile && audioRecordPerission && internetPerission) {
+
+
+                        // write your logic here
+
+                        loadImmagini();
+
+                        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh);
+                        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                            @Override
+                            public void onRefresh() {
+                                swipeRefresh.setRefreshing(true);
+
+                                new android.os.Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+
+                                        swipeRefresh.setRefreshing(false);
+                                        loadImmagini();
+                                    }
+                                }, 100);
+                            }
+                        });
+
+
+                    } else
+                        requestPermissions(
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                                        Manifest.permission.READ_PHONE_STATE,
+                                        Manifest.permission.INTERNET,
+                                        Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                PERMISSIONS_MULTIPLE_REQUEST);
+                }
+        }
+    }
+
 
 }
